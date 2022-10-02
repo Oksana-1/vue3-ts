66a3164dc1e5e80a52f65fb05ba2fb54b1d6ffe3
oksana-1 <4438488@gmail.com>
2022-10-02 12:33:52 +0300

    Using ref with Ref type

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,11 +1,21 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from "@/types/Emoji";
+import { ref } from "vue";
+import type { Ref } from "vue";
+
+const text = ref("");
+const emoji: Ref<Emoji | null> = ref(null);
+
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+    <textarea
+      v-model="text"
+      placeholder="New Journal Entry for danielkelly_io"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
       <span>0 / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
